69dd7681b54b66040477f3cc82cb26c9ef21e761
Arief <ariefnhidayah@gmail.com>
2021-05-23 13:46:30 +0700

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index');
 const mediaRouter = require('./routes/media');
@@ -11,6 +12,7 @@ const mediaRouter = require('./routes/media');
 const app = express();
 
 app.use(logger('dev'));
+app.use(methodOverride('_method'))
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
@@ -19,5 +21,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/media', mediaRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
